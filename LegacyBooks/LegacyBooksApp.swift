fcce4f4fea086ff20af793eeb1ce0a98f400ef0b
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-30 10:37:41 -0600

    Initial Commit

---
+++ LegacyBooks/LegacyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyBooksApp.swift
+//  LegacyBooks
+//
+//  Created by J. DeWeese on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LegacyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
