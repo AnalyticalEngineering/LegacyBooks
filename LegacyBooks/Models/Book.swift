5012bbf8c2c7455c2b09e2609ae514a44bbb02ca
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-01 11:24:53 -0600

    refactored and added new 'recommendedBy' property to data base

--- LegacyBooks/Models/Book.swift
+++ LegacyBooks/Models/Book.swift
@@ -16,9 +16,10 @@ class Book {
     var dateAdded:  Date
     var dateStarted: Date
     var dateCompleted:  Date
-    var summary: String
+    var synopsis: String
     var rating: Int?
     var status: Status.RawValue
+    var recommendedBy: String
     
     init(
         title: String,
@@ -26,18 +27,20 @@ class Book {
         dateAdded: Date = Date.now,
         dateStarted: Date = Date.distantPast,
         dateCompleted: Date = Date.distantPast,
-        summary: String = "",
+        synopsis: String = "",
         rating: Int? = nil,
-        status: Status = .onShelf
+        status: Status = .onShelf,
+        recommendedBy: String = ""
     ) {
         self.title = title
         self.author = author
         self.dateAdded = dateAdded
         self.dateStarted = dateStarted
         self.dateCompleted = dateCompleted
-        self.summary = summary
+        self.synopsis = synopsis
         self.rating = rating
         self.status = status.rawValue
+        self.recommendedBy = recommendedBy
     }
     
     var icon: Image {
